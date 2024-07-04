86b7757a9b54fed4178f5556e5df054183bdcabe
harshhrai01@gmail.com <harshhrai01@gmail.com>
2024-07-04 15:49:09 +0530

    Episode_01

--- app.js
+++
@@ -1,8 +0,0 @@
-// <!-- Create first H1 tag and inject into root of the html using react  -->
-  const heading =  React.createElement("h1", {id:"heading"}, "My-React-App");
-  const image = React.createElement("img", {id:"image", src:"./image/visiting card.png"})
-  const section = React.createElement("div", {id:"container"},[image , heading] );
-  const root = ReactDOM.createRoot(document.getElementById("root"));
-  root.render(section);
-console.log(section);
-
